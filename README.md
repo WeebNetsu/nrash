b513abc871b093feed3479267996266f3ca8c64d
WeebNetsu <stephenvdw7777@gmail.com>
2021-10-08 19:24:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# NRASH
+Nrash is a simple tool to work with your trash folder. This is my custom implementation of trash-cli, using Nim.
---
+++ src/nrash.nim
@@ -0,0 +1,63 @@
+import os, times
+
+from strutils import find, replace
+
+let TRASH_PATH: string = getHomeDir() & ".local/share/Trash/"
+
+proc writeTrashInfo(originalFileName, trashFileName: string) =
+    let filePath = originalFileName.replace(" ", "%20")
+    var time: string = $getTime()
+    time = time[0 ..< time.find("+")]
+    writeFile(TRASH_PATH & "info/" & trashFileName & ".trashinfo", "[Trash Info]\nPath=" & filePath & "\nDeletionDate=" & time & "\n")
+
+
+proc generateFileTrashName(file: string, isFile: bool): string =
+    var
+        fileNum: int = 1
+        notified: bool = false
+        trashFileName: string
+        exists: bool
+
+    if isFile:
+        trashFileName = splitFile(file)[1] & splitFile(file)[2]
+        exists = fileExists(TRASH_PATH & trashFileName)
+    else:
+        trashFileName = file
+        exists = dirExists(TRASH_PATH & trashFileName)
+
+    while exists:
+        fileNum += 1
+
+        if isFile:
+            trashFileName = splitFile(file)[1] & "." & $fileNum & splitFile(file)[2]
+            exists = fileExists(TRASH_PATH & "files/" & trashFileName)
+        else:
+            trashFileName = file & "." & $fileNum
+            exists = dirExists(TRASH_PATH & "files/" & trashFileName)
+
+        if fileNum >= 10000 and not notified:
+            echo "You have more than 10000 files with the same name in your recycle bin."
+            echo "Moving files to trash might become slow if you do not delete them!"
+            notified = true
+            sleep(5000)
+        
+    return trashFileName
+
+if paramCount() > 0:
+    for parameter in 1 .. paramCount():
+        let selectedFile: string = getCurrentDir() & "/" & paramStr(parameter)
+
+        # make sure the file/folder exists
+        if dirExists(selectedFile):
+            let trashFileName = generateFileTrashName(paramStr(parameter), false)
+            selectedFile.moveDir(TRASH_PATH & "files/" & trashFileName)
+            writeTrashInfo(selectedFile, trashFileName)
+        elif fileExists(selectedFile):
+            let trashFileName = generateFileTrashName(paramStr(parameter), true)
+            selectedFile.moveFile(TRASH_PATH & "files/" & trashFileName)
+            writeTrashInfo(selectedFile, trashFileName)
+        else:
+            echo "Invlid file name or file does not exist."
+            quit()
+
+        echo "Moved to trash."
