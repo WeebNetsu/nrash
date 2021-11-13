564d087ce98c1ebc4ffd2a43f300d77ada6a51f8
WeebNetsu <stephenvdw7777@gmail.com>
2021-11-13 12:13:53 +0200

    Added help flag and docs

--- src/common.nim
+++ src/common.nim
@@ -1,20 +1,31 @@
-import tables
+import tables, std/strformat
 from os import getHomeDir, paramStr, paramCount
+from terminal import setForegroundColor, resetAttributes, ForegroundColor
 
 let
     TRASH_PATH*: string = getHomeDir() & ".local/share/Trash/"
     TRASH_INFO_PATH*: string = TRASH_PATH & "info/"
     TRASH_FILES_PATH*: string = TRASH_PATH & "files/"
 
-proc checkFlags*(FLAGS: Table[string, bool]): Table[string, bool] =
-    var tbl = FLAGS
+# will set flags if any passed in
+proc checkFlags*(FLAGS: Table[string, tuple[selected: bool, desc: string]]): Table[string, tuple[selected: bool, desc: string]] =
+    var tbl: Table[string, tuple[selected: bool, desc: string]] = FLAGS
 
     if paramCount() > 0:
         for count in 1 .. paramCount():
             if tbl.hasKey(paramStr(count)):
-                tbl[paramStr(count)] = true
+                tbl[paramStr(count)].selected = true
             else:
                 echo "Invalid flag '", paramStr(count), "' provided."
                 quit()
 
     return tbl
+
+proc displayHelp*(name: string, desc: string, FLAGS: Table[string, tuple[selected: bool, desc: string]]) =
+    setForegroundColor(ForegroundColor.fgWhite)
+
+    echo &"{name} -> {desc}"
+    for flag in FLAGS.keys:
+        echo &"\t{flag}\t->\t{FLAGS[flag].desc}"
+
+    stdout.resetAttributes() # reset terminal colors & stuff
--- src/nrash_list.nim
+++ src/nrash_list.nim
@@ -5,13 +5,6 @@ from system import quit
 
 import common
 
-let FLAGS: Table[string, bool] = checkFlags({ "--all": false, "--col": false }.toTable())
-
-var
-    fileCount: int = 0
-    allFileDetails: seq[tuple[number: int, filePath: string, kind: PathComponent]]
-    no_flags = false
-
 proc displayFiles(kind: PathComponent, path: string, number: int) =
     if kind == pcFile:
         setForegroundColor(ForegroundColor.fgCyan)
@@ -25,48 +18,62 @@ proc displayFiles(kind: PathComponent, path: string, number: int) =
 
     stdout.resetAttributes() # reset terminal colors & stuff
 
-for kind, path in walkDir(TRASH_FILES_PATH):
-    fileCount += 1
+proc main() =
+    let FLAGS: Table[string, tuple[selected: bool, desc: string]] = checkFlags({
+        "--all": (false, "Display all items in trash, do not wait for a command"), 
+        # "--col": (false, "Display all items in trash (in columns)"), 
+        "--help": (false, "Display help"), 
+        }.toTable())
 
-    if FLAGS["--all"]:
-        displayFiles(kind, path, fileCount)
-    else:
-        allFileDetails.add((fileCount, path, kind))
-        noFlags = true
+    var
+        fileCount: int = 0
+        allFileDetails: seq[tuple[number: int, filePath: string, kind: PathComponent]]
+        no_flags = false
 
-        if fileCount >= 200:
-            echo "Limit of viewable files is 200."
+    for kind, path in walkDir(TRASH_FILES_PATH):
+        fileCount += 1
+
+        if FLAGS["--all"].selected: # we only handle ONE flag at a time
+            displayFiles(kind, path, fileCount)
+        elif FLAGS["--help"].selected:
+            displayHelp("nrash-list", "List items in trash", FLAGS)
             break
+        else:
+            allFileDetails.add((fileCount, path, kind))
+            noFlags = true
 
-if noFlags:
-    var fileNum: int = 1
-    while fileNum <= fileCount:
-        var file = allFileDetails[fileNum]
-        displayFiles(file.kind, file.filePath, fileNum)
-        fileNum += 1
+    if noFlags:
+        var fileNum: int = 1
 
-        if ((fileNum mod 10 == 0)) or (fileNum == fileCount - 1):
-            echo "Controls:\n\tn - next\t\tp - previous\n\tq - Quit\t\t1 - show details of first item"
-            let choice: string = readLine(stdin)
-            try:
-                let item: int = choice.parseInt()
-                if item >= fileCount:
-                    echo "No file with number ", item, " found."
-                    fileNum -= 10 # for now will work, but change so it doesn't display the files again
-                else:
-                    echo "Chosen File: ", allFileDetails[item]
-            except ValueError: # if user entered a string
-                if choice.toLowerAscii() == "p":
-                    if fileNum >= 20:
-                        fileNum -= 20
-                        echo "Going back"
+        while fileNum <= fileCount:
+            var file = allFileDetails[fileNum]
+            displayFiles(file.kind, file.filePath, fileNum)
+            fileNum += 1
+
+            if ((fileNum mod 10 == 0)) or (fileNum == fileCount - 1):
+                echo "Controls:\n\tn - next\t\tp - previous\n\tq - Quit" # \t\t1 - show details of first item
+                let choice: string = readLine(stdin)
+
+                try:
+                    let item: int = choice.parseInt()
+                    if item >= fileCount:
+                        echo "No file with number ", item, " found."
+                        fileNum -= 10 # for now will work, but change so it doesn't display the files again
                     else:
-                        echo "Can't go back, you're already at the start!"
-                elif choice.toLowerAscii() == "q":
-                    quit()
+                        echo "Chosen File: ", allFileDetails[item]
+                except ValueError: # if user entered a string
+                    if choice.toLowerAscii() == "p":
+                        if fileNum >= 20:
+                            fileNum -= 20
+                            echo "Going back"
+                        else:
+                            echo "Can't go back, you're already at the start!"
+                    elif choice.toLowerAscii() == "q":
+                        quit()
 
+    if FLAGS["--all"].selected:
+        echo "Total of ", fileCount, " files/folders"
 
-if FLAGS["--all"]:
-    echo "Total of ", fileCount, " files/folders"
+main()
 
 stdout.resetAttributes() # reset terminal colors & stuff once program exists
